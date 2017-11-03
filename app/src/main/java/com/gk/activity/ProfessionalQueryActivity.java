33e1cc93224e7321572cb9e5912f5e9e06878d1c
sujiangming <subenjiang@hotmail.com>
2017-11-03 14:49:08 +0800

    完成了搜索框

--- app/src/main/java/com/gk/activity/ProfessionalQueryActivity.java
+++ app/src/main/java/com/gk/activity/ProfessionalQueryActivity.java
@@ -1,10 +1,18 @@
 package com.gk.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.support.v7.widget.SearchView;
 import android.widget.TextView;
 
 import com.gk.R;
@@ -12,8 +20,11 @@ import com.gk.adpater.ProfessionalParentAdapter;
 import com.gk.beans.FirstBean;
 import com.gk.beans.SecondBean;
 import com.gk.beans.ThirdBean;
+import com.zhy.adapter.abslistview.CommonAdapter;
+import com.zhy.adapter.abslistview.ViewHolder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -35,10 +46,18 @@ public class ProfessionalQueryActivity extends SjmBaseActivity implements Expand
     TextView tvLevel2;
     @BindView(R.id.iv_search)
     ImageView ivSearch;
+    @BindView(R.id.ll_search)
+    LinearLayout llSearch;
+    @BindView(R.id.lv_zy_query)
+    ListView listView;
+    @BindView(R.id.searchview)
+    SearchView searchView;
 
     private ArrayList<FirstBean> mDatas = new ArrayList<FirstBean>();
     private ProfessionalParentAdapter mAdapter;
     private String faultLevel = "01"; //默认为本科
+    private List<String> stringList = new ArrayList<>();
+    private CommonAdapter<String> adapter;
 
     @Override
     public int getResouceId() {
@@ -51,7 +70,25 @@ public class ProfessionalQueryActivity extends SjmBaseActivity implements Expand
         initView();
     }
 
-    @OnClick({R.id.iv_back, R.id.iv_search, R.id.tv_level_1, R.id.tv_level_2})
+    private void initQueryData() {
+        for (int i = 0; i < 20; ++i) {
+            stringList.add("电气工程及其自动化" + i);
+        }
+        listView.setAdapter(adapter = new CommonAdapter<String>(this, R.layout.professional_query_item, stringList) {
+            @Override
+            protected void convert(ViewHolder viewHolder, String item, int position) {
+                viewHolder.setText(R.id.tv_zy_name, item);
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                toast("当前点击第" + i + "个专业：" + stringList.get(i));
+            }
+        });
+    }
+
+    @OnClick({R.id.iv_back, R.id.iv_search, R.id.tv_level_1, R.id.tv_level_2, R.id.back_image})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_back:
@@ -64,9 +101,71 @@ public class ProfessionalQueryActivity extends SjmBaseActivity implements Expand
                 tvLevel2Click();
                 break;
             case R.id.iv_search:
+                showSearch();
+                break;
+            case R.id.back_image:
+                hideSearch();
                 break;
 
         }
+    }
+
+    private TranslateAnimation mShowAction;
+    private TranslateAnimation mHiddenAction;
+
+    private void showSearch() {
+        mShowAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                -1.0f, Animation.RELATIVE_TO_SELF, 0.0f);
+        mShowAction.setDuration(250);
+        llSearch.setAnimation(mShowAction);
+        if (llSearch.getVisibility() == View.GONE) {
+            llSearch.setVisibility(View.VISIBLE);
+        }
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            private String TAG = getClass().getSimpleName();
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "onQueryTextSubmit = " + s);
+                if (searchView != null) {
+                    // 得到输入管理对象
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        // 这将让键盘在所有的情况下都被隐藏，但是一般我们在点击搜索按钮后，输入法都会乖乖的自动隐藏的。
+                        imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0); // 输入法如果是显示状态，那么就隐藏输入法
+                    }
+                }
+                searchView.clearFocus(); // 不获取焦点
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(TAG, "onQueryTextChange = " + s);
+                initQueryData();
+                return true;
+            }
+        });
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                stringList.clear();
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void hideSearch() {
+        mHiddenAction = new TranslateAnimation(Animation.RELATIVE_TO_SELF,
+                0.0f, Animation.RELATIVE_TO_SELF, 0.0f,
+                Animation.RELATIVE_TO_SELF, 0.0f, Animation.RELATIVE_TO_SELF,
+                -1.0f);
+        mHiddenAction.setDuration(250);
+        llSearch.setAnimation(mHiddenAction);
+        llSearch.setVisibility(View.GONE);
     }
 
     private void tvLevel1Click() {
