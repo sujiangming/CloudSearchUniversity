a8c8c9ec889c87db758b907f0388d964264113bb
sujiangming <subenjiang@hotmail.com>
2017-12-29 16:23:09 +0800

    2017.12.29 16:25 完成了帮助中心页面；

--- app/src/main/java/com/gk/mvp/view/activity/AboutActivity.java
+++ app/src/main/java/com/gk/mvp/view/activity/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.gk.mvp.view.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.gk.R;
 import com.gk.mvp.view.custom.TopBarView;
+import com.gk.tools.PackageUtils;
 
 import butterknife.BindView;
 
@@ -14,6 +16,8 @@ import butterknife.BindView;
 public class AboutActivity extends SjmBaseActivity {
     @BindView(R.id.top_bar)
     TopBarView topBar;
+    @BindView(R.id.tv_version)
+    TextView tv_version;
 
     @Override
     public int getResouceId() {
@@ -23,6 +27,7 @@ public class AboutActivity extends SjmBaseActivity {
     @Override
     protected void onCreateByMe(Bundle savedInstanceState) {
         setTopBar(topBar, "关于云寻校", 0);
+        tv_version.setText("云寻校V" + PackageUtils.getVersionName(this));
     }
 
 }
