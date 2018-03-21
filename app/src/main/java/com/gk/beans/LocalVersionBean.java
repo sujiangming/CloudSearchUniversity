be0841a317695eee158f363e08797a785bbcdf7f
sujiangming <subenjiang@hotmail.com>
2018-03-21 17:23:45 +0800

    2018.03.21 17:20 修复个人信息设置头像无法显示问题和修改了启动页的里面的自动登录变成了验证码形式的自定登录，改变了是否显示星特性页面的判断逻辑；

---
+++ app/src/main/java/com/gk/beans/LocalVersionBean.java
@@ -0,0 +1,37 @@
+package com.gk.beans;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Unique;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * Created by JDRY-SJM on 2017/10/10.
+ */
+@Entity
+public class LocalVersionBean {
+    @Id(autoincrement = false)
+    private Long id;
+    @Unique
+    private String versionName;
+    @Generated(hash = 1944092114)
+    public LocalVersionBean(Long id, String versionName) {
+        this.id = id;
+        this.versionName = versionName;
+    }
+    @Generated(hash = 892610013)
+    public LocalVersionBean() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getVersionName() {
+        return this.versionName;
+    }
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+}
